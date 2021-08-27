b3a9e086fb98b36a4a28bf6876cf1154036a880c
Lucas Class <lucasclass94@gmail.com>
2021-08-26 23:25:51 -0300

    css e js hoje

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*===RELÓGIO===*/
+const hour = document.getElementById('clock-hour'),
+    minutes = document.getElementById('clock-minutes'),
+    seconds = document.getElementById('clock-seconds')
+        
+const clock = () => {
+    let date = new Date()
+
+    let hh = date.getHours() * 30,
+        mm = date.getMinutes() * 6,
+        ss = date.getSeconds() * 6
+}
+
+    // ADICIONAR A ROTAÇÃO DOS ELEMENTOS
+
+
+    /*===RELÓGIO E TEXTO DE DATA===*/
